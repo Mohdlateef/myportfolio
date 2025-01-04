c4c1fdf50c645087cfad2e7f9bf644b8f9224a11
mohdlateef <lateefahbaba123@gmail.com>
2025-01-04 21:52:35 +0530

    tech complete

---
+++ src/components/Experience.tsx
@@ -0,0 +1,10 @@
+
+export default function Experience() {
+    return (
+        <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+            <h2 className="my-20 text-center text-4xl">Technologies</h2>
+
+
+        </div>
+    )
+}
---
+++ src/components/Technologies.tsx
@@ -0,0 +1,34 @@
+import { IoLogoJavascript } from "react-icons/io";
+import { SiTypescript } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaNode } from "react-icons/fa";
+import { SiMongodb } from "react-icons/si";
+export default function Technologies() {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+      <h2 className="my-20 text-center text-4xl">Technologies</h2>
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <IoLogoJavascript className="text-5xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiTypescript className="text-5xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaReact className="text-5xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          < RiTailwindCssFill className="text-5xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaNode className="text-5xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiMongodb className="text-5xl text-cyan-400" />
+        </div>
+
+      </div>
+    </div>
+  )
+}
