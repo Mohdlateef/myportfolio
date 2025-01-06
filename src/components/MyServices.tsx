24a59d307b084827fb232619b00a7c6114abd74f
mohdlateef <lateefahbaba123@gmail.com>
2025-01-06 22:19:32 +0530

    services complete

--- src/components/MyServices.tsx
+++ src/components/MyServices.tsx
@@ -1,4 +1,6 @@
 import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { FaHive } from "react-icons/fa";
+import { MdComputer } from "react-icons/md";
 import { My_services } from "../constants"
 
 export default function MyServices() {
@@ -7,7 +9,7 @@ export default function MyServices() {
             <h2 className="my-20 text-center text-4xl">My Serivices </h2>
             <div>
                 <div className="flex justify-between items-center flex-wrap">
-                    <div className="flex flex-col justify-center tracking-tight items-center p-2  rounded-lg lg:w-1/4 gap-1 shadow-md shadow-gray-700">
+                    <div className="flex flex-col justify-center tracking-tight items-center p-2  rounded-lg lg:w-1/4 gap-1 shadow-md shadow-gray-700 p-3 hover:bg-slate-500  hover:scale-105">
                         <HiOutlineDesktopComputer className="text-white text-7xl my-5" />
 
 
@@ -15,18 +17,18 @@ export default function MyServices() {
                         <p className="text-white">{My_services}</p>
 
                     </div>
-                    <div className="flex flex-col justify-center tracking-tight items-center  p-2  rounded-lg lg:w-1/4 gap-1 shadow-md shadow-gray-700">
-                        <HiOutlineDesktopComputer className="text-white text-7xl my-5" />
+                    <div className="flex flex-col justify-center tracking-tight items-center rounded-lg lg:w-1/4 gap-1 shadow-md shadow-gray-700 p-3  hover:bg-slate-500  hover:scale-105">
+                        <FaHive className="text-white text-7xl my-5" />
 
 
-                        <h3 className="text-white text-xl font-bold">Frontend Development</h3>
+                        <h3 className="text-white text-xl font-bold">Backend Development</h3>
                         <p className="text-white">{My_services}</p>
 
-                    </div><div className="flex flex-col justify-center tracking-tight items-center p-2  rounded-lg lg:w-1/4 gap-1 shadow-md shadow-gray-700">
-                        <HiOutlineDesktopComputer className="text-white text-7xl my-5" />
+                    </div><div className="flex flex-col justify-center tracking-tight items-center p-2  rounded-lg lg:w-1/4 gap-1 shadow-md shadow-gray-700 p-3 hover:bg-slate-500  hover:scale-105">
+                        <MdComputer className="text-white text-7xl my-5" />
 
 
-                        <h3 className="text-white text-xl font-bold">Frontend Development</h3>
+                        <h3 className="text-white text-xl font-bold">Responsive Web Desing</h3>
                         <p className="text-white">{My_services}</p>
 
                     </div>
---
+++ src/components/Projects.tsx
@@ -0,0 +1,9 @@
+
+export default function Projects() {
+    return (
+        <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+
+
+        </div>
+    )
+}
